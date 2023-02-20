c7aba14a32d9e9940be8894dc60a025633452730
sidsharma2002 <rooter@rooters-MacBook-Pro.local>
2023-02-20 10:41:36 +0530

    bgthreadposter deadlock tests

--- app/src/main/java/com/example/androidconcepts/common/BgThreadPoster.kt
+++ app/src/main/java/com/example/androidconcepts/common/BgThreadPoster.kt
@@ -1,12 +1,14 @@
 package com.example.androidconcepts.common
 
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.SynchronousQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
 
-class BgThreadPoster {
-    private val taskExecutor: ThreadPoolExecutor = ThreadPoolExecutor(
+open class BgThreadPoster {
+    protected open val taskExecutor: ExecutorService = ThreadPoolExecutor(
         /* corePoolSize = */ 3,
         /* maximumPoolSize = */ Int.MAX_VALUE,
         /* keepAliveTime = */ 60,
@@ -14,7 +16,17 @@ class BgThreadPoster {
         /* workQueue = */ SynchronousQueue()
     )
 
-    fun post(runnable: Runnable): Future<*> {
+    open fun post(runnable: Runnable): Future<*> {
         return taskExecutor.submit(runnable)
     }
+}
+
+class BgThreadPosterDualThreaded : BgThreadPoster() {
+    override val taskExecutor: ExecutorService = Executors.newFixedThreadPool(2)
+}
+
+class BgThreadPosterSingleThreaded : BgThreadPoster() {
+    // default override shows taskExecutor: ExecutorService get() = ...
+    // it will return new executor each time. this mistake wasted 20mins :/
+    override val taskExecutor: ExecutorService = Executors.newSingleThreadExecutor()
 }
